3203813c88e6a240e38e3c1eb3bfefd52f327d37
Ismael <maelandrade@yahoo.com.br>
2021-11-02 17:58:30 -0300

    Criado JsonView para editar um usuario, password e image, bem como seus respectivos endponts

--- authuser/src/main/java/br/com/ismadrade/authuser/controllers/AuthenticationController.java
+++ authuser/src/main/java/br/com/ismadrade/authuser/controllers/AuthenticationController.java
@@ -5,6 +5,7 @@ import br.com.ismadrade.authuser.enums.UserStatus;
 import br.com.ismadrade.authuser.enums.UserType;
 import br.com.ismadrade.authuser.models.UserModel;
 import br.com.ismadrade.authuser.service.UserService;
+import com.fasterxml.jackson.annotation.JsonView;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -23,7 +24,8 @@ public class AuthenticationController {
     UserService userService;
 
     @PostMapping("/signup")
-    public ResponseEntity<Object> registerUser(@RequestBody UserDto userDto){
+    public ResponseEntity<Object> registerUser(@RequestBody
+                                               @JsonView(UserDto.UserView.RegistrationPost.class) UserDto userDto){
 
         if(userService.existsByUsername(userDto.getUsername())){
             return ResponseEntity.status(HttpStatus.CONFLICT).body("Error: Username is Already Taken!");
