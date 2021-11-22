0dc197b8ce5585b1c7c3d768ce26f1a84dbf3bec
Ismael <maelandrade@yahoo.com.br>
2021-11-21 21:16:43 -0300

    Adicionado controller para lesson

--- course/src/main/java/br/com/ismadrade/course/services/LessonService.java
+++ course/src/main/java/br/com/ismadrade/course/services/LessonService.java
@@ -1,4 +1,17 @@
 package br.com.ismadrade.course.services;
 
+import br.com.ismadrade.course.models.LessonModel;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 public interface LessonService {
+    LessonModel save(LessonModel lessonModel);
+
+    Optional<LessonModel> findLessonIntoModel(UUID moduleId, UUID lessonId);
+
+    void delete(LessonModel lessonModel);
+
+    List<LessonModel> findAllByModule(UUID moduleId);
 }
--- course/src/main/java/br/com/ismadrade/course/services/impl/LessonServiceImpl.java
+++ course/src/main/java/br/com/ismadrade/course/services/impl/LessonServiceImpl.java
@@ -1,13 +1,38 @@
 package br.com.ismadrade.course.services.impl;
 
+import br.com.ismadrade.course.models.LessonModel;
 import br.com.ismadrade.course.repositories.LessonRepository;
 import br.com.ismadrade.course.services.LessonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class LessonServiceImpl implements LessonService {
 
     @Autowired
     LessonRepository lessonRepository;
+
+    @Override
+    public LessonModel save(LessonModel lessonModel) {
+        return lessonRepository.save(lessonModel);
+    }
+
+    @Override
+    public Optional<LessonModel> findLessonIntoModel(UUID moduleId, UUID lessonId) {
+        return lessonRepository.findLessonIntoModel(moduleId, lessonId);
+    }
+
+    @Override
+    public void delete(LessonModel lessonModel) {
+        lessonRepository.delete(lessonModel);
+    }
+
+    @Override
+    public List<LessonModel> findAllByModule(UUID moduleId) {
+        return lessonRepository.findAllLessonsIntoModule(moduleId);
+    }
 }
