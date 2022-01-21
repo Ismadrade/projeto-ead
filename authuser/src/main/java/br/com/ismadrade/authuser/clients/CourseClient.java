025a1fd4f6a37a0ea955e8d2dd0e1411d9fe9134
Ismael Andrade <maelandrade@yahoo.com.br>
2022-01-21 19:47:17 -0300

    Adicionando resilience4j na api authuser

--- authuser/src/main/java/br/com/ismadrade/authuser/clients/CourseClient.java
+++ authuser/src/main/java/br/com/ismadrade/authuser/clients/CourseClient.java
@@ -3,6 +3,8 @@ package br.com.ismadrade.authuser.clients;
 import br.com.ismadrade.authuser.dtos.CourseDto;
 import br.com.ismadrade.authuser.dtos.ResponsePageDto;
 import br.com.ismadrade.authuser.services.UtilsService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -16,6 +18,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -32,6 +35,8 @@ public class CourseClient {
     @Value("${ead.api.url.course}")
     String REQUEST_URL_COURSE;
 
+    //@Retry(name = "retryInstance",fallbackMethod = "retryfallback")
+    @CircuitBreaker(name = "circuitbreakerInstance")
     public Page<CourseDto> getAllCoursesByUser(UUID userId, Pageable pageable){
         List<CourseDto> searchResult = null;
         String url =  REQUEST_URL_COURSE + utilsService.createUrl(userId, pageable);
@@ -48,5 +53,17 @@ public class CourseClient {
         log.info("Ending request /courses userId {}", userId);
         return new PageImpl<>(searchResult);
     }
+
+//    public Page<CourseDto> circuitbreakerfallback(UUID userId, Pageable pageable, Throwable t){
+//        log.error("Inside retry retryfallback, cause - {}", t.toString());
+//        List<CourseDto> searchResult = new ArrayList<>();
+//        return new PageImpl<>(searchResult);
+//    }
+//
+//    public Page<CourseDto> retryfallback(UUID userId, Pageable pageable, Throwable t){
+//        log.error("Inside retry retryfallback, cause - {}", t.toString());
+//        List<CourseDto> searchResult = new ArrayList<>();
+//        return new PageImpl<>(searchResult);
+//    }
 
 }
