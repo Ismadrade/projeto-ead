1dc424146e75a52d862062631b7105d8efab0a9c
Ismael Andrade <maelandrade@yahoo.com.br>
2022-02-20 16:01:08 -0300

    Configurado security para autenticar com banco de dados

---
+++ authuser/src/main/java/br/com/ismadrade/authuser/config/security/UserDetailsServiceImpl.java
@@ -0,0 +1,23 @@
+package br.com.ismadrade.authuser.config.security;
+
+import br.com.ismadrade.authuser.models.UserModel;
+import br.com.ismadrade.authuser.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserModel userModel = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + username));
+        return UserDetailsImpl.build(userModel);
+    }
+}
--- authuser/src/main/java/br/com/ismadrade/authuser/repositories/UserRepository.java
+++ authuser/src/main/java/br/com/ismadrade/authuser/repositories/UserRepository.java
@@ -1,13 +1,17 @@
 package br.com.ismadrade.authuser.repositories;
 
 import br.com.ismadrade.authuser.models.UserModel;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<UserModel, UUID>, JpaSpecificationExecutor<UserModel> {
-    boolean existsByUsername(String username);
 
+    boolean existsByUsername(String username);
     boolean existsByEmail(String email);
+    @EntityGraph(attributePaths = "roles", type = EntityGraph.EntityGraphType.FETCH)
+    Optional<UserModel> findByUsername(String username);
 }
